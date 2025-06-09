7f2704e76c7b2c055175c94f8a3ceb95e96ec472
Robert Pito Sanchez <robsanchez124@gmail.com>
2025-06-08 20:45:57 -0400

    created a workflows/yml file and added base to vite-config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+//if this string is equal to production, then the base is /wayback/, otherwise it is /
+const base = process.env.NODE_ENV === "production" ? "/wayback/" : "/";
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: base,
   plugins: [react()],
 });
