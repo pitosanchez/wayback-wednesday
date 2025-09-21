63efd8284be10f9299cf27b5d63001fee3a28adb
Robert Pito Sanchez <robsanchez124@gmail.com>
2025-09-21 11:39:34 -0400

    chore(favicon): replace Vite icon with GBo The Pro icon sitewide

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import gboIconUrl from './assets/images/svgbo.svg?url'
+
+// Set favicon to GBo The Pro icon
+const ensureFavicon = () => {
+  let link = document.querySelector('link[rel="icon"]') as HTMLLinkElement | null
+  if (!link) {
+    link = document.createElement('link')
+    link.rel = 'icon'
+    document.head.appendChild(link)
+  }
+  link.type = 'image/svg+xml'
+  link.href = gboIconUrl
+}
+
+ensureFavicon()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
